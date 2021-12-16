ba879bbf44507ae6069a90f26f9c71d3a05e352b
LeeRippon <lee.dale.rippon@gmail.com>
2021-12-15 17:29:11 -0800

    Add files via upload

---
+++ GitColab_Test.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+from time import time 
+
+def load():
+    print('Loading kiln data...')
+    t1 = time()
+    df = pd.read_csv('d_Eastover_B1_Aug31_Avg.csv') 
+    l = len(df)
+    t2 = time()
+    print('Loading time is', t2-t1, 'seconds')
+    
+    return df, l
